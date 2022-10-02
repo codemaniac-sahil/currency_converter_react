55abb021e0dd9fc6d0accd610379e883c1b139e0
Sushant Kumar <sushantkumar@Sushants-MacBook-Air.local>
2022-10-02 22:40:19 +0530

    fixed for deploying checks

--- src/components/GitIcon.js
+++ src/components/GitIcon.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const cssStyle = {
-    float:'right',
-
-}
-
-
 const GitIcon = (props) => {
 
     const link=props.link!=null?props.link : "https://github.com/codemaniac-sahil/currency_converter_react"; 
@@ -13,7 +7,7 @@ const GitIcon = (props) => {
     
     return ( 
         
-        <a href={link} target="_blank" style={{float:'right'}}>
+        <a href={link} target="_blank" style={{float:'right'}} rel="github">
         <svg width = "10vw" height ="10vw"viewBox="0 0 250 250">
         <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"/>
         <path style={{transformOrigin: "130px 106px"}} className="octo-arm" d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" />
