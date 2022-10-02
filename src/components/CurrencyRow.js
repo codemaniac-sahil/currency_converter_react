582a2afc547e4f0e9da79012ad339575145ea3f7
Najeeb <najeebthangalkp@gmail.com>
2022-10-02 16:10:32 +0530

    Added country flag [fixes #6]

--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
+import { CountryData } from "./CountryFlag";
 
 function CurrencyRow(props) {
-  const{
+  const {
     currencyOption,
     selectedCurrency,
     onChangeCurrency,
     onChangeAmount,
-    amount
-  }=props
+    amount,
+  } = props;
   return (
     <div>
-
-        <input type='number' className='input' value={amount} onChange={onChangeAmount}/>
-        <select value={selectedCurrency} onChange={onChangeCurrency}>
-          {currencyOption.map(option=>(
-            <option key={option} value={option}>{option}</option>
-          ))}
-            
-        </select>
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOption.map((option) => (
+          <option key={option} value={option}>
+            {CountryData.find((item) => item.currency === option).flag} &ensp;
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
-  )
+  );
 }
 
-export default CurrencyRow
+export default CurrencyRow;
